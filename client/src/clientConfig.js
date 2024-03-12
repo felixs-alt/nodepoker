a03cfa9c91137ae25c3e3af13a6d6bce37fd0442
felixs-alt <felixplays25@gmail.com>
2024-03-12 13:52:33 +0000

    qdw

--- client/src/clientConfig.js
+++ client/src/clientConfig.js
@@ -6,7 +6,7 @@ const config = {
   socketURI:
     process.env.NODE_ENV === 'production'
       ? process.env.REACT_APP_SERVER_URI
-      : `http://${window.location.hostname}:5000/`,
+      : `http://${window.location.hostname}:5000`,
 };
 
 export default config;
